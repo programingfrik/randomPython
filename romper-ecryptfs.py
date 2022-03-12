9ab063253132c7c05a19205002ebd5533098b3ce
Pablo Mercader Alcántara <programingfrik@gmail.com>
2022-03-12 12:05:01 -0400

    Agregando un script para tratar de adivinar un password de ecryptfs.

---
+++ romper-ecryptfs.py
@@ -0,0 +1,53 @@
+#!/usr/bin/ipy
+# -*- coding: utf-8 -*-
+
+# Esto es un script para tratar de romper un passphrase de ecryptfs usando fuerza bruta.
+
+import sys
+import pexpect
+
+config = {
+    "rtdicc": "../diccionario.txt",
+    "ecryptcomm": "ecryptfs-unwrap-passphrase"
+}
+
+def probarPalabra(fichpassph, palabra):
+    global config
+    print("Probando \"{}\"".format(palabra))
+    expdor = pexpect.spawn("{0} {1}" .format(config["ecryptcomm"], fichpassph))
+    expdor.expect("Passphrase: ")
+    expdor.sendline(palabra)
+    resp = expdor.expect(["Error: ", "[a-z0-9]{10,}"])
+    if resp == 0:
+        print("Falló la palabra \"{}\"".format(palabra))
+        return False
+    else:
+        print("La clave \"{}\" fue exitosa.".format(palabra))
+        return True
+
+def ataque_diccionario(fichpassph):
+    global config
+    print("Haciendo el ataque del diccionario")
+    # lee el diccionario
+    roto = False
+    with open(config["rtdicc"], "r") as fich:
+        for linea in fich:
+            # prueba cada palabra en el diccionario.
+            roto = probarPalabra(linea.replace("\n", ""))
+            if roto:
+                print("Entcontré el passphrase la palabra \"{}\"!!".format(palabra))
+                break
+        else:
+            print("Se probaron todas las palabras disponibles"
+                  + " y no se ha podido romper el passphrase")
+
+def main():
+    print("Tratando de romper el passpharse {}.".format(fichero))
+    if sys.argv != 2:
+        print("El uso correcto es \"romper-ecryptfs.py"
+              + " /home/pedro/.ecryptfs/wrapped-passphrase\"")
+        sys.exit(1)
+    ataque_diccionario(sys.argv[1])
+
+if __name__ == "__main__":
+    main()
