0003765b66d3056f384c4889b2ffce25779f31e0
Pablo Mercader Alcántara <programingfrik@gmail.com>
2026-08-05 08:00:28 -0400

    Creando una función que calculé las coordenada de una recta perpendicular a otra recta dada.

---
+++ calcular-perpendicular.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Esto es un script para crear y probar una función python que calcule
+# las coordenadas de una recta normal a una recta descrita por los
+# puntos A y B, a una distancia H.
+
+def calcularPRN(A, B, H):
+    pass
+
+def pruebaCalculo(A, B, H, C):
+    Cc = calcularPRN(A, B, H)
+    if (C == Cc):
+        print("Pasó!")
+    else:
+        print("No pasó")
+
+def main():
+    
+    pass
+    
+
+if __name__ == "__main__":
+    main()
+
