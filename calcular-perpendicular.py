c5e7570ae4041bf8bafae0c696c496ccfc57b914
Pablo Mercader Alcántara <pmercader@banreservas.com>
2026-08-12 13:32:44 -0400

    Ahora funciona el calculo de la perpendicular. Hay una rotación que no funciona como yo creía.

--- calcular-perpendicular.py
+++ calcular-perpendicular.py
@@ -20,6 +20,7 @@ def normalizar(v):
     global X, Y
     modulo = math.sqrt(v[X] ** 2 + v[Y] ** 2)
     vn = v[X] / modulo, v[Y] / modulo
+    return vn
 
 def calcular_perpendicular(a, b, h):
     global X, Y
@@ -27,25 +28,14 @@ def calcular_perpendicular(a, b, h):
     # Calcula el punto medio
     med = ((b[X] + a[X]) / 2, (b[Y] + a[Y]) / 2)
 
-    # Calculando la pendiente
-    # if b[X] != a[X]:
-    #     m = (b[Y] - a[Y]) // (b[X] - a[X])
-    # elif b[Y] > a[Y]:
-    #     m = 0; # + infinito
-    # elif b[Y] < a[Y]:
-    #     m = 0; # - infinito
-
     # Calcula el "vertor real"
     vr = b[X] - a[X], b[Y] - a[Y]
 
     # Normaliza el vector real
     vrn = normalizar(vr)
 
-    # Rota 90 grados al vector real normalizado
-    # TODO: ¿Radianes o grados?
-    vrnr = rotar_punto(vrn, 90)
-
-    # avr = math.atan2(vr[Y], vr[X])
+    # Rota 90 grados, o pi / 2, el vector real normalizado
+    vrnr = rotar_punto(vrn, math.pi / 2)
 
     # Solo queda multiplicarlo por h para que tenga la longitud
     # solicitada y trasladarlo al sitio del punto medio.
@@ -75,6 +65,7 @@ def main():
     probar_calculo((5, 0), (15, 0), 5, (10, 5), "prueba evidente 1")
     probar_calculo((15, 0), (5, 0), 5, (10, -5), "prueba evidente 2")
     probar_calculo((5, 5), (5, 15), 5, (0, 10), "prueba evidente 3")
+    probar_calculo((5, 5), (15, 15), 5, (15, 5), "prueba evidente diagonal 45 grados")
 
 if __name__ == "__main__":
     main()
